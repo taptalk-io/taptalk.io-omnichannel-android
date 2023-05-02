68cae828458c5b41aa0da6fb73b22173885e6dc1
Kevin Reynaldo <kevin@tapta.lk>
2023-05-02 16:29:51 +0700

    • Fixed API call loop on initialization failed • Added TapTalkLiveListener callbacks

--- taptalklive/src/main/java/io/taptalk/taptalklive/Interface/TapTalkLiveInterface.java
+++ taptalklive/src/main/java/io/taptalk/taptalklive/Interface/TapTalkLiveInterface.java
@@ -1,7 +1,21 @@
 package io.taptalk.taptalklive.Interface;
 
+import android.app.Activity;
+
+import io.taptalk.TapTalk.Model.TAPMessageModel;
+import io.taptalk.taptalklive.API.Model.ResponseModel.TTLErrorModel;
+
 public interface TapTalkLiveInterface {
+
     void onInitializationCompleted();
+
+    void onInitializationFailed(TTLErrorModel error);
+
+    void onNotificationReceived(TAPMessageModel message);
+
     void onCloseButtonInCreateCaseFormTapped();
+
     void onCloseButtonInCaseListTapped();
+
+    void onTaskRootChatRoomClosed(Activity activity);
 }
--- taptalklive/src/main/java/io/taptalk/taptalklive/Listener/TapTalkLiveListener.java
+++ taptalklive/src/main/java/io/taptalk/taptalklive/Listener/TapTalkLiveListener.java
@@ -1,10 +1,28 @@
 package io.taptalk.taptalklive.Listener;
 
+import static io.taptalk.taptalklive.Const.TTLConstant.TapTalkInstanceKey.TAPTALK_INSTANCE_KEY;
+
+import android.app.Activity;
+
+import io.taptalk.TapTalk.Helper.TapTalk;
+import io.taptalk.TapTalk.Model.TAPMessageModel;
+import io.taptalk.taptalklive.API.Model.ResponseModel.TTLErrorModel;
 import io.taptalk.taptalklive.Interface.TapTalkLiveInterface;
 
 public abstract class TapTalkLiveListener implements TapTalkLiveInterface {
     @Override
     public void onInitializationCompleted() {
+
+    }
+
+    @Override
+    public void onInitializationFailed(TTLErrorModel error) {
+
+    }
+
+    @Override
+    public void onNotificationReceived(TAPMessageModel message) {
+        TapTalk.showTapTalkNotification(TAPTALK_INSTANCE_KEY, message);
     }
 
     @Override
@@ -14,6 +32,11 @@ public abstract class TapTalkLiveListener implements TapTalkLiveInterface {
 
     @Override
     public void onCloseButtonInCaseListTapped() {
+
+    }
+
+    @Override
+    public void onTaskRootChatRoomClosed(Activity activity) {
 
     }
 }
